70b1aeb15172c0300271bdf76c6e30301ef3b988
Alex Shut <proxiv.game@gmail.com>
2019-06-24 22:52:51 +0300

    попытка

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-unused-vars */
 import './styles/main.css';
+import Model from './model';
+import View from './view';
+import Controller from './controller';
+// import { save, load } from './helpers';
 
-console.log('Hi');
+// const state = load();
+
+const model = new Model();
+const view = new View();
+const controller = new Controller(model, view);
